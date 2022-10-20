35571ffae7c452fba3e6dce635b50c12fffefb21
nsikak udonyah <nsikak080@gmail.com>
2022-10-21 00:50:58 +0100

    singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,45 @@
 #include "lists.h"
+#include <string.h>
+#include <stdio.h>
+
 /**
- * _strlen - finds the length of a string
- * @str: string to find the length of
+ * add_node_end - add a node at the end of a linked list
+ * @head: The character to print
+ * @str: string for the new node
  *
- * Return: length of string
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		;
-
-	return (i);
-}
-/**
- * add_node_end - adds node to the end of a linked list
- * @head: first node of the list to be substituted
- * @str: string to be duplicated
- * Return: number of elements in a list
+ * Return: new node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
+	list_t *current;
+	list_t *new_node;
+	int c;
 
-	if (str == NULL)
-		return (NULL);
+	current = *head;
+	while (current && current->next != NULL)
+		current = current->next;
+
+	for (c = 0; str[c] != '\0'; c++)
+		;
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
-
+	}
 	new_node->str = strdup(str);
-	new_node->len = _strlen(new_node->str);
-
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+	new_node->len = c;
 	new_node->next = NULL;
-	if ((*head) == NULL)
-	{
-		(*head) = new_node;
-		return (new_node);
-	}
-	/* set temp to head, move temp, set temp->next to new_node */
-	temp = (*head);
-	/* move temp->next not NULL then set temp->next to new_node */
-	while (temp->next != NULL)
-		temp = temp->next;
-	temp->next = new_node;
+
+	if (current)
+		current->next = new_node;
+	else
+		*head = new_node;
 	return (new_node);
 }
