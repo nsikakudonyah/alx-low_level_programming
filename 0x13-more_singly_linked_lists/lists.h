bb8c822ad33c51574c95b0036a91531e3bb5d3b9
nsikak udonyah <nsikak080@gmail.com>
2022-10-26 01:37:23 +0100

    new lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,18 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stddef.h>
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* structure */
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ *
  */
 typedef struct listint_s
 {
@@ -16,6 +20,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Prototypes of functions */
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -29,7 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
 
-#endif /* _LISTS_H_ */
+#endif /* LISTS_H */
